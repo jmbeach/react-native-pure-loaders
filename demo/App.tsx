658e785669938cc03abffaf358ee07c85d4034cc
Jared Beach <jaredbeachdesign@gmail.com>
2020-02-27 06:22:52 -0600

    props - adds working demo

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Loaders from './lib/index'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Loaders.Ring color="#b3b3b3" />
     </View>
   );
 }
